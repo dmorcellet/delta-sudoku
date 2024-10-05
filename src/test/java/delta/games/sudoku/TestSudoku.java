d056d6632e510bbab980c6f284a25f04e5681d59
Damien Morcellet <dmorcellet@free.fr>
2024-10-05 12:55:49 +0200

    Use JUnit 5 instead of JUnit 3

--- src/test/java/delta/games/sudoku/TestSudoku.java
+++ src/test/java/delta/games/sudoku/TestSudoku.java
@@ -1,38 +1,37 @@
 package delta.games.sudoku;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import java.net.URL;
 
-import junit.framework.Assert;
-import junit.framework.TestCase;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 import delta.common.utils.url.URLTools;
 
 /**
  * Test for sudoko solver.
  * @author DAM
  */
-public class TestSudoku extends TestCase
+@DisplayName("Sudoku")
+class TestSudoku
 {
-  /**
-   * Constructor.
-   */
-  public TestSudoku()
-  {
-    super("Sudoku");
-  }
-
   /**
    * Test cell fill.
    */
-  public void testFillSudoku()
+  @Test
+  void testFillSudoku()
   {
     SudokuGrid grid=new SudokuGrid();
     int bigX=1, bigY=0, x=2, y=0, value=5;
     grid.setValueForCell(bigX,bigY,x,y,value);
-    Assert.assertEquals(value,grid.getGrid(bigX,bigY).getValueForCell(x,y).intValue());
+    assertEquals(value,grid.getGrid(bigX,bigY).getValueForCell(x,y).intValue());
     try
     {
       grid.setValueForCell(bigX,bigY,x,y,value);
-      Assert.assertTrue(false);
+      assertTrue(false);
     }
     catch (IllegalStateException e)
     {
@@ -44,20 +43,22 @@ public class TestSudoku extends TestCase
   /**
    * Test grid loading.
    */
-  public void testReadSudoku()
+  @Test
+  void testReadSudoku()
   {
     SudokuGrid grid=loadGrid("testSudokuFacile");
-    Assert.assertNotNull(grid);
+    assertNotNull(grid);
     grid.dump(System.out);
   }
 
   /**
    * Test grid solving.
    */
-  public void testSolveSudoku()
+  @Test
+  void testSolveSudoku()
   {
     SudokuGrid grid=loadGrid("testSudokuFacile");
-    Assert.assertNotNull(grid);
+    assertNotNull(grid);
     grid.dump(System.out);
     SudokuSolver solver=new SudokuSolver();
     solver.solve(grid);
@@ -67,14 +68,15 @@ public class TestSudoku extends TestCase
   {
     URL url=URLTools.getFromClassPath(gridName+".txt",TestSudoku.class);
     SudokuGrid grid=SudokuReader.from(url);
-    Assert.assertNotNull(grid);
+    assertNotNull(grid);
     return grid;
   }
 
   /**
    * Test grid dump.
    */
-  public void testDump()
+  @Test
+  void testDump()
   {
     SudokuGrid grid=new SudokuGrid();
     grid.dump(System.out);
